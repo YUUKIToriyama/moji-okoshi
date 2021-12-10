108c6366d4d65e811fac69ae2acf32bf1b3be386
YUUKIToriyama <github@toriyama.dev>
2021-12-10 21:54:02 +0900

    betaバージョンを使うように設定

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,30 @@ import fs from 'fs/promises';
 
 export const flacToText = async (fileName: string) => {
 	const audioFile = await fs.readFile(fileName);
-	const client = new speech.SpeechClient({
+	const client = new speech.v1p1beta1.SpeechClient({
 		keyFile: './key.json' // 「IAMと管理」「サービスアカウント」「キー」
 	});
-	const [response] = await client.recognize({
+	const [operation] = await client.longRunningRecognize({
 		audio: {
 			content: audioFile.toString('base64')
 		},
 		config: {
 			languageCode: 'ja-JP',
 			encoding: 'FLAC',
-			audioChannelCount: 2,
+			audioChannelCount: 1,
 			useEnhanced: true,
 			model: 'phone_call',
-			enableAutomaticPunctuation: true
+			enableAutomaticPunctuation: true,
+			metadata: {
+				microphoneDistance: "MIDFIELD",
+				audioTopic: "演劇についてのインタビュー"
+			},
+			enableWordConfidence: true,
+			enableSpeakerDiarization: true,
+			diarizationSpeakerCount: 2,
 		}
 	});
+	const [response] = await operation.promise();
 	return response.results.map(result => {
 		const alternatives = result.alternatives.sort((a, b) => {
 			return (a.confidence < b.confidence) ? 1 : -1;
